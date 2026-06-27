dbb80455cddc05d4fbaa002864b543b6236df4b8
MatejTvYt <dusica.stamenik@gmail.com>
2026-06-27 22:56:59 +0200

    Delete tag-mod-template-1.21.1 directory

--- tag-mod-template-1.21.1/src/main/java/com/example/tagmod/TagTickHandler.java
+++
@@ -1,9 +0,0 @@
-package com.example.tagmod;
-
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-
-public class TagTickHandler {
-    public static void register() {
-        ServerTickEvents.END_SERVER_TICK.register(TagGame::tick);
-    }
-}
